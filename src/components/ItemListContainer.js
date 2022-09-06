46689e3eb01b1e850d0675b253b981a73f83876f
Lautaro Perez Lastreto <lautaroperezlastreto@gmail.com>
2022-09-06 19:11:16 -0300

    contador

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import ItemCount from './ItemCount/ItemCount'
 import Title from './Title'
 
 const ItemListContainer = () => {
+
+    const onAdd = (quantity) =>{
+        console.log(`Compraste ${quantity} unidades`)
+
+    }
   return (
+    <>
     <Title greeting = 'Ena'/>
-  )
+    <ItemCount initial={1} stock={5} onAdd={onAdd} />
+    </>
+  );
 }
 
 export default ItemListContainer
