7678f917e70451d8afa89af5fbc21cf8660e824e
Lautaro Perez Lastreto <lautaroperezlastreto@gmail.com>
2022-09-07 17:09:10 -0300

    le falta

---
+++ src/components/Item/Item.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Item.css'
+
+const Item = () => {
+  return (
+    <a href='' className='film'>
+        <img src='https://static.wikia.nocookie.net/cine/images/f/f8/Fight-Club-1999.jpg/revision/latest?cb=20121017183904' alt=''/>
+        <p>Fight Club</p>
+    </a>
+  )
+}
+
+export default Item
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ItemCount from './ItemCount/ItemCount'
 import Title from './Title'
+import ItemList from './ItemList'
 
-const ItemListContainer = () => {
+
+const films = [
+    {id: 1, image: "https://static.wikia.nocookie.net/cine/images/f/f8/Fight-Club-1999.jpg/revision/latest?cb=20121017183904", title: "Fight Club"},
+    {id: 2, image: "https://pics.filmaffinity.com/Seven_Se7en-498520078-large.jpg", title: "Seven"},
+    {id: 3, image: "https://pics.filmaffinity.com/El_precio_del_poder-798722679-large.jpg", title: "Scarface"}
+];
+
+export const ItemListContainer = () => {
 
     const onAdd = (quantity) =>{
         console.log(`Compraste ${quantity} unidades`)
@@ -12,6 +20,7 @@ const ItemListContainer = () => {
     <>
     <Title greeting = 'Ena'/>
     <ItemCount initial={1} stock={5} onAdd={onAdd} />
+    <ItemList/>
     </>
   );
 }
